e8e451b4599e6f453f28b9b0e5aed33e5082acfd
mdmurad71 <mdmurad1685@gmail.com>
2023-05-25 04:49:42 -0700

    register login add

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:191',
+            'email' => 'required|email|max:191|unique:users,email',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'validation_errors' => $validator->messages(),
+            ]);
+        } else {
+            $users = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+    }
+    return response()->json([
+        'status' => 200,
+        'userName' => $users->name,
+        'message' => 'resgister successfully',
+    ]);
+}
+
+
+function Login(Request $request){
+    $validator = Validator::make($request->all(), [
+        'email' => 'required',
+        'password' => 'required'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'validator_errors' => $validator->message(),
+        ]);
+    } else {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'status' => '401',
+                'message' => 'invalid credentials'
+            ]);
+        } else {
+
+
+
+            return response()->json([
+                'status' => 200,
+                'userName' => $user->name,
+                'message' => 'Loged in successfully',
+            ]);
+        }
+    }
+}
+
+
+}
